2a9df790d41e7097c2be5a9e5bd0b37bd376d597
UMariangel <mariangelurbinaa@gmail.com>
2026-05-30 11:53:13 +0100

    Correccion de paquete

--- src/main/java/com/aninode/backend/services/SecurityConfig.java
+++ src/main/java/com/aninode/backend/services/SecurityConfig.java
@@ -1,4 +1,4 @@
-package com.aninode.backend.config;
+package com.aninode.backend.services;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
